3e81ee3617a4ab3c9dbe6372e1a5a2bea8a0b310
Reshma62 <mernstack2024@gmail.com>
2024-07-13 11:36:26 +0600

    add

--- src/app/utils/getPaginateData.ts
+++ src/app/utils/getPaginateData.ts
@@ -1,6 +1,6 @@
 import { Document, Model, FilterQuery } from "mongoose";
 
-interface Query {
+export interface IQuery {
   page?: string;
   limit?: string;
   sort?: string;
@@ -15,17 +15,15 @@ interface PaginateResult<T> {
 
 export const getPaginateDataService = async <T extends Document>(
   model: Model<T>,
-  query: Query,
+  query: IQuery,
   searchFields: string[]
 ): Promise<PaginateResult<T>> => {
-  // Parse page and limit to integers, using nullish coalescing to provide defaults
   const page = parseInt(query?.page || "1", 10);
   const limit = parseInt(query?.limit || "10", 10);
-  const sort = query?.sort || "-createdAt"; // Default sort by descending order of createdAt
+  const sort = query?.sort || "-createdAt";
 
   const { page: _, limit: __, sort: ___, search, ...otherQuery } = query;
 
-  // Construct the search query
   const searchQuery: FilterQuery<T> = search
     ? ({
         $or: searchFields.map((field) => ({
@@ -34,24 +32,27 @@ export const getPaginateDataService = async <T extends Document>(
       } as FilterQuery<T>)
     : {};
 
-  // Combine the search query with other queries
+  const filterQuery: Partial<FilterQuery<T>> = {};
+
+  Object.keys(otherQuery).forEach((key) => {
+    if (key !== "price" && key !== "limit" && otherQuery[key] !== undefined) {
+      (filterQuery as any)[key] = otherQuery[key];
+    }
+  });
+
   const combinedQuery: FilterQuery<T> = {
-    ...otherQuery,
+    ...filterQuery,
     ...searchQuery,
     isDeleted: { $ne: true },
   };
 
-  // Prepare the query
   let queryBuilder = model
     .find(combinedQuery)
-    .sort(sort) // Apply sorting
-    .skip((page - 1) * limit) // Skip the appropriate number of documents
-    .limit(limit); // Limit the number of documents to be returned
+    .sort(sort)
+    .skip((page - 1) * limit)
+    .limit(limit);
 
-  // Execute the query
   const data = await queryBuilder.exec();
-
-  // Calculate the total count of matching documents
   const count = await model.countDocuments(combinedQuery);
 
   return { data, count };
